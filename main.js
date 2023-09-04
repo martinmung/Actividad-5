a824d6f95e1ce9232666c7dc574691c22ba0fafd
Martin <martinfx21@gmail.com>
2023-09-04 15:21:50 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var arreglo = [1, 4, 6, 10, 22, 55, 46, 2, 5, 0]
+
+for(var i = 0; i < 10; i++){
+    if(i>3){
+        console.log(i)
+    }
+}
+
+
+var arregloVacio = []
+
+for(var i=1; i<6; i++){
+    arregloVacio.push(i)
+    console.log(`${i} ha sido agregado al arreglo`)
+}
+
+console.log(arregloVacio)
